e949dd9fdddfd818175dc7e3f190f799d591184d
Raul lama <raullama90@gmail.com>
2024-10-11 14:05:49 +0200

    Inici

---
+++ Desktop/FitnessCasesNoves/src/main/java/spdvi/fitnesscasesnoves/FitnessCasesNoves.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package spdvi.fitnesscasesnoves;
+
+/**
+ *
+ * @author Rulox
+ */
+public class FitnessCasesNoves {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
